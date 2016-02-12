2368fa98d2ac668d21a891987c657e7d1605be1b
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-12 12:34:00 -0500

    Add test shelter edit and delete to test suite

--- tests/base.py
+++ tests/base.py
@@ -15,7 +15,7 @@ class BaseTestCase(TestCase):
         db.create_all()
         db.session.add(Adoptors(name='Testname'))
         db.session.add(Adoptors(name='Billtest'))
-        db.session.add(Shelter(name="Testshelter", address="123 Fake st.", city="Fake", state="FK", zipCode="12345", website="http://test.com", maximum_capacity=10, current_capacity=5))
+        db.session.add(Shelter(name="Testshelter", address="123 Fake st.", city="Fake", state="Florida", zipCode="12345", website="http://test.com", maximum_capacity=10, current_capacity=5))
         db.session.add(Puppy(name="Testpup", shelter_id=1, gender="male", dateOfBirth=datetime.datetime.now(),picture="http://testpup.com",weight=1, show=True))
         db.session.add(Profile(puppy_id=1, breed="Testbreed",description="This is a test description", specialNeeds="Testblind"))
         
