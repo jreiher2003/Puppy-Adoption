6f9b57d57c5ebd0c62abfc186889d446ca5fac23
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-12 16:59:09 -0500

    Refactor test names

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -18,7 +18,7 @@ class FlaskTestCase(BaseTestCase):
         self.assertIn(b'Testshelter', response.data)
 
     # Ensure that the shelter profile loads correctly
-    def test_shelter_profile_page_loads(self):
+    def test_index_shelter_profile_page_loads(self):
         response = self.client.get('/1/testshelter/page/1')
         self.assertIn(b'Testshelter', response.data)
         self.assertIn(b'123 Fake st.', response.data)
@@ -29,12 +29,12 @@ class FlaskTestCase(BaseTestCase):
         self.assertTrue(str(shelter) == '<name>: Testshelter')
 
     # Ensure that puppy-profile correctly
-    def test_puppy_profile(self):
+    def test_index_puppy_profile(self):
         response = self.client.get('/1/testshelter/profile/1', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     # Ensure that the login page loads correctly
-    def test_puppy_profile_page_loads(self):
+    def test_index_puppy_profile_page_loads(self):
         response = self.client.get('/1/testshelter/profile/1')
         self.assertIn(b'Testshelter', response.data)
         self.assertIn(b'Testpup', response.data)
@@ -44,25 +44,25 @@ class FlaskTestCase(BaseTestCase):
         self.assertIn(b'This is a test description', response.data)
 
     # Ensure that Flask was set up correctly
-    def test_adopt_puppy_page(self):
+    def test_index_adopt_puppy_page(self):
         response = self.client.get('/1/testshelter/profile/1/adopt/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     # Ensure that the login page loads correctly
-    def test_adopt_puppy_page_loads(self):
+    def test_index_adopt_puppy_page_loads(self):
         response = self.client.get('/1/testshelter/profile/1/adopt/')
         self.assertIn(b'Who do you want to adopt <u>Testpup</u>?', response.data)
 
 
     # Ensure that test adpot page 2 set up correctly
-    def test_adopt_2_puppy_page(self):
+    def test_index_adopt_2_puppy_page(self):
         response = self.client.get('/1/testshelter/profile/2/adopt/2/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Are you sure you want <mark>Billpup</mark> to be adopted by <mark>Billtest</mark>?', response.data)
     
      
       # Ensure that list-adoptions response is correct
-    def test_list_adoptions(self):
+    def test_index_list_adoptions(self):
         response = self.client.get('/list-adoptions', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'List of past adoptions', response.data)
