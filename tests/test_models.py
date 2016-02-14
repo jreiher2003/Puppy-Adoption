f6e3cce4837f5b86ad52b95eb77fd89e61a23c7c
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-14 11:36:21 -0500

    Add coveage to test suite,
    Attempt 1 to up the coverage on models.py-- unsuccessfull.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,10 +1,27 @@
 import unittest
 import datetime
 from base import BaseTestCase
+from app import app,db
 from app.models import Adoptors, Shelter, Puppy, Profile, AdoptorsPuppies
 
 
 class TestModelCase(BaseTestCase):
+
+    def test_adoptor_db(self):
+        self.assertTrue(
+            db.session.query(Adoptors).filter(Adoptors.name=='Testname').first()
+            is not None)
+
+    def test_model_update(self):
+        """Model modification should modify model"""
+        model = db.session.query(Adoptors).filter(Adoptors.name=='Testname').first()
+        model.name ='New name'
+        db.session.add(model)
+        db.session.commit()
+        self.assertTrue(
+            db.session.query(Adoptors).filter(Adoptors.name=='New name').first()
+            is not None)
+
 
 	def test_model_tablenames(self):
 		# adoptor = Adoptors.query.all()
