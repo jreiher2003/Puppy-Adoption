52cc7353675e2a3d8ae0f46d86b43e390251e3b8
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-16 14:15:39 -0500

    Configure flask_mail to app -working
    Next step send test emails through app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask 
+from flask_mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__) 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+mail = Mail(app)
 db = SQLAlchemy(app)
 
 from app import views, models
