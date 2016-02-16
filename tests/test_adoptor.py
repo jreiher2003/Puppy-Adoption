d09b20877c7a5a886892c6d8df05f5c6891f15c4
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-15 19:59:13 -0500

    Check tests are acurate

--- tests/test_adoptor.py
+++ tests/test_adoptor.py
@@ -5,30 +5,29 @@ from app.models import Adoptors
 
 
 class TestAdoptorCase(BaseTestCase):
-	 # Ensure that /adoptors response correctly
-    def test_adoptors(self):
-        response = self.client.get('/adoptors', content_type='html/text')
+
+    def test_adoptors_page_loads(self):
+        """ Tests that /adoptors page loads """
+        response = self.client.get('/adoptors',content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'A list of potenial Adoptors', response.data)
         self.assertIn(b'Testname', response.data)
 
-    # Ensure that the /adoptors page loads correctly
-    def test_adoptors_page_loads(self):
-        response = self.client.get('/adoptors',content_type='html/text')
-        self.assertIn(b'A list of potenial Adoptors', response.data)
-        self.assertIn(b'Testname', response.data)
-
-      # Ensure that /new-shelterresponse is correct
+      
     def test_adoptors_new_adoptor_page_loads(self):
+        """ Tests that /new-adoptor page loads """
         response = self.client.get('/new-adoptor', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Become an Adoptor', response.data)
-
+    
     def test_adoptors_add_new(self):
         response = self.client.post('/new-adoptor', data=dict(name='Jefftest'), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<strong>Just created</strong> a new adoptor named <u>Jefftest</u>', response.data)
 
+    def test_adoptors_add_new_test_error(self):
+        response = self.client.post('/new-adoptor', data=dict(name=''), follow_redirects=True)
+        self.assertIn(b'This field is required.', response.data)
 
     def test_adoptors_from_index_add_new(self):
         response = self.client.post('/', data=dict(name='Jefftest1'), follow_redirects=True)
@@ -48,7 +47,6 @@ class TestAdoptorCase(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<strong>You just</strong> deleted <u>Testname</u>', response.data)
         
-
     def test_adoptors_edit_status(self):
         response = self.client.get('/adoptors/profile/1/edit/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
